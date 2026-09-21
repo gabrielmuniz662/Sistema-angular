8bceb287fb1dcfa49896cc68397cd6b2e3239a3a
gabrielmuniz662 <272995@unifio.edu.br>
2026-09-21 23:16:13 +0000

    exercicio 3

---
+++ angular/src/app/exercicios/exercicio3/exercicio3.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio3',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercicio3.html',
+  styleUrl: './exercicio3.scss'
+})
+export class Exercicio3 {
+  imagemProduto: string = 'https://angular.io/assets/images/logos/angular/angular.svg';
+  descricaoImagem: string = 'Logo do Angular';
+}
--- angular/src/app/exercicios/exercicios-module.ts
+++ angular/src/app/exercicios/exercicios-module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { ExerciciosRoutingModule } from './exercicios-routing-module';
 import { Exercicio1 } from './exercicio1/exercicio1';
 import { Exercicio2 } from './exercicio2/exercicio2';
+import { Exercicio3 } from './exercicio3/exercicio3';
 
 @NgModule({
-  imports: [CommonModule, ExerciciosRoutingModule, Exercicio1, Exercicio2],
+  imports: [CommonModule, ExerciciosRoutingModule, Exercicio1, Exercicio2, Exercicio3],
 })
 export class ExerciciosModule {}
